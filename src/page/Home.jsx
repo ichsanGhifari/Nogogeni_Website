8f117b28bb80d72140d784ec8e119f0469cef5fc
gayuh <gkoma444@gmail.com>
2021-07-15 12:42:21 +0700

    working on home transition

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -59,13 +59,12 @@ const Home=()=> {
             <SwiperSlide>
                 <img alt='' className='backg' src={bg1}></img>
                 <div className='z-10 flex flex-col mx-auto p-10 mt-14'>    
-                    <div className='flex flex-col space-y-0 md:-space-y-24'>
+                    <div className='flex flex-col space-y-0 md:-space-y-2'>
                         <div className='flex text-title flex-col  text-center italic'>
                             <h3 style={{letterSpacing: "0.25em"}} 
-                            className='text-2xl md:text-5xl'>NOGOGENI RACING</h3>
-                            <h1 className='text-5xl md:text-9xl' >MARK I</h1>
+                            className='text-2xl md:text-5xl'>NOGOGENI V</h3>
                         </div>
-                        <img className='z-10 mx-auto' width="900px" alt ='' src={et1}></img>
+                        <img className='z-10 mx-auto' width="800px" alt ='' src={et1}></img>
                     </div>
                     <Homebutton />
                 </div>
