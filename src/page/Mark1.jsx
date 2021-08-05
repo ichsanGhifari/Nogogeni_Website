8f3b57624fe7b5b9ea1b18958e4dbd37f6396dfd
gayuh <gkoma444@gmail.com>
2021-08-06 02:29:28 +0700

    working on home transition button

--- src/page/Mark1.jsx
+++ src/page/Mark1.jsx
@@ -1,9 +1,27 @@
 import Navbar from '../components/Navbar/index'
-
+import f1 from '../images/home/f1.png'
+import rangka1 from '../images/home/rankga/Ranka1Top.png'
+import rangka2bot from '../images/home/rankga/Ranka1Bot.png'
 const Mark1 = () =>{
     return(
-        <section className='h-screen bg-bghero bg-cover bg-no-repeat'>
+        <section className='h-screen bg-bghero bg-cover bg-no-repeat overflow-hidden'>
              <Navbar />
+             <div className='h-max absolute -mt-48 ml-20'>
+                 <img className='transform -rotate-90' width={'950px'} alt='' src={f1}></img>
+             </div>
+             <div className='slicer'>
+                 <div className='flex flex-col'>
+                     <img alt='' src={rangka1} className=''></img>
+                     <img alt='' src={rangka2bot} className=''></img>
+                 </div>
+             </div>
+             <style jsx>{`
+                .slicer{
+                    position:absolute;
+                    top:32%;
+                    left:5%;
+                }
+             `}</style>
         </section>
     );
 }
