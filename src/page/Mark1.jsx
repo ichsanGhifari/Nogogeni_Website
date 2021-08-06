4391f522b30a7591b6b21b7e6ec16b3d7c742b27
gayuh <gkoma444@gmail.com>
2021-08-06 11:53:49 +0700

    bug on hooker

--- src/page/Mark1.jsx
+++ src/page/Mark1.jsx
@@ -2,7 +2,11 @@ import Navbar from '../components/Navbar/index'
 import f1 from '../images/home/f1.png'
 import rangka1 from '../images/home/rankga/Ranka1Top.png'
 import rangka2bot from '../images/home/rankga/Ranka1Bot.png'
+import React, { useState } from 'react';
+
 const Mark1 = () =>{
+    const [slider, setSlider] = useState(true);
+
     return(
         <section className='h-screen bg-bghero bg-cover bg-no-repeat overflow-hidden'>
              <Navbar />
@@ -10,9 +14,9 @@ const Mark1 = () =>{
                  <img className='transform -rotate-90' width={'950px'} alt='' src={f1}></img>
              </div>
              <div className='slicer'>
-                 <div className='flex flex-col'>
-                     <img alt='' src={rangka1} className=''></img>
-                     <img alt='' src={rangka2bot} className=''></img>
+                 <div className='flex flex-col cursor-pointer'>
+                    <img alt='' src={rangka1} className={`${slider?"border-2 border-white":""}`} onClick={()=>setSlider(true)}></img>
+                    <img alt='' src={rangka2bot} className={`${slider?"":"border-2 border-white"}`} onClickC={()=>setSlider(false)}></img>
                  </div>
              </div>
              <style jsx>{`
