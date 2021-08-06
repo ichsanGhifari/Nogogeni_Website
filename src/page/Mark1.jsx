1175e4b253a665d10067b52cc31ea17387431ba6
gayuh <gkoma444@gmail.com>
2021-08-06 18:43:27 +0700

    working on content swap

--- src/page/Mark1.jsx
+++ src/page/Mark1.jsx
@@ -3,35 +3,68 @@ import f1 from '../images/home/f1.png'
 import rangka1 from '../images/home/rankga/Ranka1Top.png'
 import rangka2bot from '../images/home/rankga/Ranka1Bot.png'
 import back from '../icons/Back2.png'
+import steering from '../icons/Steering1.png'
+import wheel from '../icons/Wheels1.png'
+import Tires from '../icons/Tires1.png'
+import transmition from '../icons/Transmisions1.png'
+
 import React, { useState } from 'react';
 
 const Mark1 = () =>{
-    const [slider, setSlider] = useState(true);
+    const [slider, setSlider] = useState(false);
 
     return(
         <section className='h-screen bg-bghero bg-cover bg-no-repeat overflow-hidden'>
             <Navbar />
-            <div className={`left-20 relative transform-gpu duration-700 w-max ${slider?"translate-y-full top-20":"-translate-y-20 -top-20"}` }>
+            <div className={`left-20 2xl:left-60 relative transform-gpu duration-700 w-max ${slider?"translate-y-full top-20":"-translate-y-20 -top-20"}` }>
                 <img className={`transform -rotate-90`} width={'950px'} alt='' src={f1}></img>
             </div>
-            
             <div className='slicer'>
                 <div className='flex flex-col cursor-pointer'>
                 <img alt='' src={rangka1} className={`${slider?"border-2 border-white":"border-0"}`} onClick={()=>setSlider(true)}></img>
                 <img alt='' src={rangka2bot} className={`${slider?"border-0":"border-2 border-white"}`} onClick={()=>setSlider(false)}></img>
                 </div>
             </div>
-            <div className='text-white w-max right-40 inset-y-1/2 absolute px-10'>
-            <div className='flex flex-col gap-3'>
-                <div className='flex flex-row cursor-pointer gap-3'>
-                    <img src={back} alt='' className='border-2 border-white rounded-full w-8'></img>
-                    <p className='italic font-medium text-xl my-auto'>Back</p>
-                </div>
-                <div>
+            <div className='text-white w-max right-40 inset-y-1/4 absolute px-10 2xl:right-64'>
+                <div className='flex flex-col gap-3'>
+                    <div className='flex flex-row cursor-pointer gap-3'>
+                        <img src={back} alt='' className='border-2 border-white rounded-full w-8'></img>
+                        <p className='italic font-medium text-xl my-auto'>Back</p>
+                    </div>
                     <h2 className='italic text-title  '>NOGOGENI RACING <br />MARK 1</h2>
+                    <p className='text-title font-bold'>This car has a Husaberg 450 FE engine with a <br />
+                    single cylinder RON 98 fuel type. The chassis <br />
+                    structure is made of Steel Tube Spaceframe and,<br/> 
+                    with a brake system using hydraulic disc brakes.</p>
+                    <div className='flex flex-row gap-5'>
+                        <div className='flex flex-col gap-2'>
+                            <img alt='' src={steering}></img>
+                            <img alt='' src={transmition}></img>
+                        </div>
+                        <div className='flex flex-col gap-5 mt-2 italic font-bold text-title'>
+                            <p>Rack and Pinion Steering System</p>
+                            <p>Sprocket &amp; Chain with Manual Shifter</p>
+                        </div>
+                    </div>
+                    <div className='flex flex-row gap-4'>
+                        <div className='flex flex-col'>
+                            <p className='text-title italic font-bold text title'>Wheels</p>
+                            <div className='bg-title p-3 flex flex-row gap-3'>
+                                <img alt='' src={wheel}></img>
+                                <p className='italic my-auto text-black'>6,4" Wide</p>
+                            </div>
+                        </div>
+                        <div className='flex flex-col'>
+                            <p className='text-title italic font-bold text title'>Tires</p>
+                            <div className='bg-title p-3 flex flex-row gap-3'>
+                                <img alt='' src={Tires}></img>
+                                <p className='italic my-auto text-black'>180/60 R13 Hoosier</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            </div>
+            
              <style jsx>{`
                 .slicer{
                     position:absolute;
