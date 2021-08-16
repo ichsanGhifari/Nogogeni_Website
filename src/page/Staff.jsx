e4555dccbed922a0da3988c529a5e01e373ceb46
gayuh <gkoma444@gmail.com>
2021-08-16 22:16:43 +0700

    staff added

---
+++ src/page/Staff.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar"
+
+
+const Staff = () =>{
+    return(
+        <section className='h-screen bg-bgStaff bg-cover'>
+            <Navbar /> 
+
+        </section>
+    )
+}
+export default Staff
