70d6e915be48825c2f575a07f84243b26940717c
gayuh <gkoma444@gmail.com>
2021-08-19 21:41:36 +0700

    working on contact

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -1,15 +1,31 @@
-import Navbar from "../components/Navbar"
-import title from '../images/contact/contactTitle.png'
+import Navbar from "../components/Navbar";
+import title from "../images/contact/contactTitle.png";
+import gm from "../images/contact/GeneralManager.png";
 
+const Contact = () => {
+  return (
+    <section className="h-screen bg-bgContact bg-cover">
+      <Navbar />
+      <div className="top-24 absolute w-screen">
+        <img className=" md:p-0 px-7 mx-auto" alt="" src={title}></img>
+      </div>
+      <div className='w-screen absolute top-1/4'>
+        <div className=" flex flex-col mx-auto w-3/4">
+            <div className="flex flex-row w-full -space-x-24 ">
+                <div className='w-1/6 z-0'>
+                    <img src={gm} alt="" className=" z-10 rounded-full"></img>
+                </div>
+                <div className="py-5 px-28 border-2 bg-white ml-36 ">
+                    <p className='font-bold'>General Manager</p>
+                    <p>Dimas Andi Setiawan</p>
+                    <div className='flex flex-col'>
 
-const Contact =()=>{
-    return(
-        <div className='h-screen bg-bgContact bg-cover'>
-            <Navbar />
-            <div className='top-24 absolute w-screen'>
-                <img className=' md:p-0 px-7 mx-auto' alt='' src={title}></img>
+                    </div>
+                </div>
             </div>
         </div>
-    )
-}
-export default Contact
+      </div>
+    </section>
+  );
+};
+export default Contact;
