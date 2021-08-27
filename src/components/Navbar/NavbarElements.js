71fbd6778f955a86a5460e2dd92660ae8cbcc361
gayuh <gkoma444@gmail.com>
2021-08-28 00:44:35 +0700

    sidebar effect

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,116 +1,128 @@
-import styled from 'styled-components';
-import {Link as LinkR} from 'react-router-dom';
+import styled from "styled-components";
+import { Link as LinkR } from "react-router-dom";
 
 export const Nav = styled.nav`
-    height:80px;
-    /* background:black; */
-    display: absolute;
-    justify-content: center;
-    align-items: center;
-    font-size: 1rem;
-    position: sticky;
-    top:0;
-    z-index:10;
-    @media screen and (max-widht:950px){
-        transisiton:0.7s all ease;
-        height:50px;
-    }
+  height: 80px;
+  /* background:black; */
+  display: absolute;
+  justify-content: center;
+  align-items: center;
+  font-size: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  @media screen and (max-widht: 950px) {
+    transisiton: 0.7s all ease;
+    height: 50px;
+  }
+`;
+
+export const Sidebar = styled.div`
+
+  background-color: #1e1e1e;
+  width: 20rem;
+  overflow: hidden;
+  width:max-content;
+  position: fixed;
+  top: -90%;
+  left: 0;
+  transition: 0.5s ease-in-out;
+  /* transition-timing-function:cubic-bezier(1,.01,.57,.34); */
+  opacity: ${({ isOpen }) => (isOpen ? "100%" : "0%")};
+  left: ${({ isOpen }) => (isOpen ? "-9rem" : "10rem")};
+  height:${({ isOpen }) => (isOpen ? "100vh":"0")};
+  padding: ${({isOpen}) => (isOpen?"7rem 2.5rem":"0 0")};
+
 `;
 export const NavbarContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-    height: 80px;
-    z-index: 1;
-    width:100%;
-    padding:0 24px;
-    width:100%;
+  display: flex;
+  justify-content: space-between;
+  height: 80px;
+  z-index: 1;
+  width: 100%;
+  padding: 0 24px;
+  width: 100%;
 `;
 
 export const NavLogo = styled(LinkR)`
-color :#fff;
-justify-self:flex-start;
-cursor:pointer;
-font-size: 1.5rem;
-display:flex;
-align-items:center;
-margin-left:24px;
-font-weight:bold;
-text-decoration:none;
+  color: #fff;
+  justify-self: flex-start;
+  cursor: pointer;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  margin-left: 24px;
+  font-weight: bold;
+  text-decoration: none;
 `;
 
 export const MobileIcon = styled.div`
-display:none;
-@media screen and (max-width:768px){
-    display:block;
-    position:absolute;
-    top:-20px;
-    right:-25px;
-    transform: translate(-100%,60%);
-    font-size:1.8rem;
-    cursor:pointer;
-    color :#fff;
-
-}
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: -10%;
+    right: 0%;
+    transform: translate(-100%, 60%);
+    font-size: 1.8rem;
+    cursor: pointer;
+    color: #fff;
+  }
 `;
 export const NavMenu = styled.ul`
-display:flex;
-align-items:center;
-list-style:none;
-text-align:left;
+  display: flex;
+  align-items: center;
+  list-style: none;
+  text-align: left;
 
-@media screen and (max-width:768px){
-    display:none;
-}
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 
 export const NavItem = styled.li`
-height:80px;
+  height: 80px;
 `;
 
-export const NavLinks=styled(LinkR)`
-color:#fff;
-display:flex;
-align-items:center;
-text-decoration:none;
-font-style: italic;
-font-weight: normal;
-padding:0 1rem;
-height:50%;
-margin-top:1.25rem;
-cursor:pointer;
+export const NavLinks = styled(LinkR)`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  font-style: italic;
+  font-weight: normal;
+  padding: 0 1rem;
+  height: 50%;
+  margin-top: 1.25rem;
+  cursor: pointer;
 
-&.active{
-    border-bottom:10px solid #01bf71;
-}
-&:hover{
-    
-}
+  &.active {
+    border-bottom: 10px solid #01bf71;
+  }
+  &:hover {
+  }
 `;
-export const NavLinksChild=styled(LinkR)`
-color:#fff;
-display:flex;
-align-items:center;
-text-decoration:none;
-font-style: italic;
-font-weight: normal;
-height:50%;
-cursor:pointer;
+export const NavLinksChild = styled(LinkR)`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  font-style: italic;
+  font-weight: normal;
+  height: fit-content;
+  cursor: pointer;
 
-&.active{
-    border-bottom:10px solid #01bf71;
-}
-&:hover{
-    
-}
+  &.active {
+    border-bottom: 10px solid #01bf71;
+  }
+  &:hover {
+  }
 `;
 export const NavBtn = styled.nav`
-display:flex;
-align-items:center;
+  display: flex;
+  align-items: center;
 
-@media screen and (max-width:768px){
-    display:none;
-}
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
-
-
-
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,141 +1,135 @@
-import React from 'react';
-import {Nav,
-	NavLogo,
-	MobileIcon,
-	NavItem,
-	NavMenu,
-	NavLinks,
-	NavLinksChild
-} from './NavbarElements';
-import { Squash as Hamburger } from 'hamburger-react'
-import logo from '../../images/LogoNogen.png'
-import { Menu,Transition, Disclosure } from '@headlessui/react'
+import React, { useState } from "react";
+import {
+  Nav,
+  NavLogo,
+  MobileIcon,
+  NavItem,
+  NavMenu,
+  NavLinks,
+  NavLinksChild,
+  Sidebar,
+} from "./NavbarElements";
+import { Squash as Hamburger } from "hamburger-react";
+import logo from "../../images/LogoNogen.png";
+import { Menu, Transition, Disclosure } from "@headlessui/react";
+
 // import { motion } from "framer-motion"
 
+const Navbar = () => {
+  const [isOpen, setOpen] = useState(false);
 
-const Navbar = ({toggle})=>{
+  return (
+    <div className={`z-50 absolute `}>
+      <Nav className="">
+        <div className="w-screen flex flex-row md:justify-between justify-center px-5 md:py-0 py-5 ">
+          <NavLogo to="/">
+            <img alt="" src={logo} className="md:pt-0 pt-1"></img>
+          </NavLogo>
+          <MobileIcon>
+            <div className="flex flex-col  ">
+              <Sidebar className={`text-2xl mt-5 flex flex-col gap-5 `} isOpen={isOpen} >
+                <NavLinksChild to="/">Home</NavLinksChild>
+                <Disclosure>
+                  <Disclosure.Button>
+                    <h2 className="italic font-light text-left">About</h2>
+                  </Disclosure.Button>
+                  <Transition
+                    enter="transition ease-in duration-300 opacity-0  "
+                    enterFrom="transform scale-100 opacity-10 "
+                    enterTo="transform scale-100 opacity-100 "
+                    leave="transition ease-out duration-300 opacity-0 "
+                    leaveFrom="transform scale-100 opacity-10 "
+                    leaveTo="transform scale-100 opacity-0 "
+                  >
+                    <Disclosure.Panel>
+                      <div className="flex flex-col gap-3 ml-5 -mt-3 text-lg">
+                        <NavLinksChild to="/Team">Team</NavLinksChild>
+                        <NavLinksChild to="/Event">Event</NavLinksChild>
+                        <NavLinksChild to="/Staff">Staff</NavLinksChild>
+                        <NavLinksChild to="/Testimonial">
+                          Testimonial
+                        </NavLinksChild>
+                      </div>
+                    </Disclosure.Panel>
+                  </Transition>
+                </Disclosure>
+                <NavLinksChild to="/Achievment">Achievement</NavLinksChild>
+                <NavLinksChild to="/Partner">Partner</NavLinksChild>
+                <NavLinksChild to="/Gallery">Gallery</NavLinksChild>
+                <NavLinksChild to="/Contact">Contact</NavLinksChild>
+              </Sidebar>
+			  <Hamburger
+                className=""
+                toggled={isOpen}
+                toggle={setOpen}
+              ></Hamburger>
+            </div>
+          </MobileIcon>
 
-    return (
-		<div className='absolute overflow-visi z-50'>
-		<Nav>
-			<div className='w-screen flex flex-row md:justify-between justify-center px-5 md:py-0 py-5 ' >
-				<NavLogo to='/'><img alt='' src={logo} className='md:pt-0 pt-1'></img></NavLogo>
-				<MobileIcon>
-				<Disclosure>
-					<div className='flex flex-row '>
-					<Disclosure.Button className="py-2 z-30">
-						<Hamburger className='z-30'></Hamburger>
-					</Disclosure.Button>
-					<Transition
-						appear={true}
-						enter="transform-gpu transition ease-in-out duration-700"
-						enterFrom="translate-x-96 "
-						enterTo="transform-gpu translate-x-1"
-						entered="transform-gpu translate-x-spe"
-						leave="transform-gpu transition ease-in-out duration-1000 translate-x-10"
-						leaveFrom="transform-gpu translate-x-spe"
-						leaveTo="transform-gpu translate-x-96 "
-					>
-					<Disclosure.Panel className="absolute h-screen py-20 px-10 sidebar -right-5 z-50 ">
-						<div className='text-2xl mt-5 flex flex-col gap-5'>
-							<NavLinksChild to='/'>Home</NavLinksChild>
-							<Disclosure>
-								<Disclosure.Button>
-									<h2 className='italic font-light text-left'>About</h2>
-								</Disclosure.Button>
-								<Transition
-								enter="transition ease-in duration-300 opacity-0  "
-								enterFrom="transform scale-100 opacity-10 "
-								enterTo="transform scale-100 opacity-100 "
-								leave="transition ease-out duration-300 opacity-0 "
-								leaveFrom="transform scale-100 opacity-10 "
-								leaveTo="transform scale-100 opacity-0 ">
-									<Disclosure.Panel>
-									<div className='flex flex-col gap-3 ml-5 -mt-3 text-lg'>
-										<NavLinksChild to='/Team'>Team</NavLinksChild>
-										<NavLinksChild to='/Event'>Event</NavLinksChild>
-										<NavLinksChild to='/Staff'>Staff</NavLinksChild>
-										<NavLinksChild to='/Testimonial'>Testimonial</NavLinksChild>
-									</div>
-									</Disclosure.Panel>
-								</Transition>
-							</Disclosure>
-							<NavLinksChild to='/Achievment'>Achievement</NavLinksChild>
-							<NavLinksChild to='/Partner'>Partner</NavLinksChild>
-							<NavLinksChild to='/Gallery'>Gallery</NavLinksChild>
-							<NavLinksChild to='/Contact'>Contact</NavLinksChild>
-						</div>
-					</Disclosure.Panel>
-					</Transition>
-					</div>
-				</Disclosure>
-				</MobileIcon>
-
-				<NavMenu>
-					<NavItem>
-						<NavLinks to='/' className='transform translate-y-0'>Home</NavLinks>
-					</NavItem>
-							<Menu >
-								<div className='grid-cols-1 flex-grow-0 '>
-								<Menu.Button className='italic text-white px-4 h-1/2 '>About</Menu.Button>
-								<Transition
-								enter="transition duration-100 ease-out"
-								enterFrom="transform scale-0 opacity-0"
-								enterTo="transform scale-100 opacity-100"
-								leave="transition duration-75 ease-out"
-								leaveFrom="transform scale-100 opacity-100"
-								leaveTo="transform scale-0 opacity-0"
-								>	
-								<Menu.Items className=' z-50 flex-col gap-2 flex italic absolute text-left px-4 mt-2 '>
-									<Menu.Item>
-									{({ active }) => (
-										<NavLinksChild to='/Team'>Team</NavLinksChild>
-									)}
-									</Menu.Item>
-									<Menu.Item>
-									{({ active }) => (
-										<NavLinksChild to='/Event'>Event</NavLinksChild>
-									)}
-									</Menu.Item>
-									<Menu.Item>
-									{({ active }) => (
-										<NavLinksChild to='/Staff'>Staff</NavLinksChild>
-									)}
-									</Menu.Item>
-									<Menu.Item>
-									{({ active }) => (
-										<NavLinksChild to='/Testimonial'>Testimonial</NavLinksChild>
-									)}
-									</Menu.Item>
-								</Menu.Items>					
-								</Transition>
-								</div>
-							</Menu>
-						<NavItem>
-							<NavLinks to='/Achievment'>Achievement</NavLinks>
-						</NavItem>
-						<NavItem>
-							<NavLinks to='/Partner'>Partner</NavLinks>
-						</NavItem>
-						<NavItem>
-							<NavLinks to='/Gallery'>Gallery</NavLinks>
-						</NavItem>
-						<NavItem>
-							<NavLinks to='/Contact'>Contact</NavLinks>
-						</NavItem>
-				</NavMenu>
-			</div>
-		</Nav>
-		<style jsx>{`
-			.sidebar{
-				background-color:#1E1E1E;
-				width:255px;
-				overflow:hidden;
-
-				top:-30%;
-			}
-		`}</style>
-		</div>
-    )
-}
-export default Navbar
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="/" className="transform translate-y-0">
+                Home
+              </NavLinks>
+            </NavItem>
+            <Menu>
+              <div className="grid-cols-1 flex-grow-0 ">
+                <Menu.Button className="italic text-white px-4 h-1/2 ">
+                  About
+                </Menu.Button>
+                <Transition
+                  enter="transition duration-100 ease-out"
+                  enterFrom="transform scale-0 opacity-0"
+                  enterTo="transform scale-100 opacity-100"
+                  leave="transition duration-75 ease-out"
+                  leaveFrom="transform scale-100 opacity-100"
+                  leaveTo="transform scale-0 opacity-0"
+                >
+                  <Menu.Items className=" z-50 flex-col gap-2 flex italic absolute text-left px-4 mt-2 ">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <NavLinksChild to="/Team">Team</NavLinksChild>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <NavLinksChild to="/Event">Event</NavLinksChild>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <NavLinksChild to="/Staff">Staff</NavLinksChild>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <NavLinksChild to="/Testimonial">
+                          Testimonial
+                        </NavLinksChild>
+                      )}
+                    </Menu.Item>
+                  </Menu.Items>
+                </Transition>
+              </div>
+            </Menu>
+            <NavItem>
+              <NavLinks to="/Achievment">Achievement</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="/Partner">Partner</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="/Gallery">Gallery</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="/Contact">Contact</NavLinks>
+            </NavItem>
+          </NavMenu>
+        </div>
+      </Nav>
+      <style jsx>{`
+      `}</style>
+    </div>
+  );
+};
+export default Navbar;
