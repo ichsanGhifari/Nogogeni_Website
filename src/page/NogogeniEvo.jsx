941de35689f5dd78e37f0c2cd33edbaea7c7236f
gayuh <gkoma444@gmail.com>
2021-08-27 11:02:05 +0700

    home 2 done

--- src/page/NogogeniEvo.jsx
+++ src/page/NogogeniEvo.jsx
@@ -1,13 +1,13 @@
 import Navbar from "../components/Navbar/index";
 import el1 from "../images/home/Electric1.png";
-import steering from "../icons/Steering1.png";
-import breaks from "../icons/Brake1.png";
+import steering from "../icons/Steering2.png";
+import body1 from "../icons/Body1.png";
 import isometri1 from "../images/home/isometri/Isometri12.png";
 import isometri2 from "../images/home/isometri/Isometri22.png";
 import rangka1 from "../images/home/rankga/ranka2top.png";
 import rangka2bot from "../images/home/rankga/ranka2bot.png";
-import transmition from "../icons/Transmisions1.png";
-import wheel from "../icons/Wheels1.png";
+import brake from "../icons/Brake1.png";
+import chassis from "../icons/Chassis1.png";
 import back from "../icons/Back2.png";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
@@ -15,10 +15,10 @@ import { Link } from "react-router-dom";
 const NogogeniEvo = () => {
   const [slider, setSlider] = useState(false);
   return (
-    <section className="h-screen bg-bgherosecond bg-cover bg-no-repeat">
+    <section className="h-screen bg-bgherosecond bg-cover bg-no-repeat overflow-hidden">
       <Navbar />
       <div
-        className={`md:left-20 left-40 mt-16 2xl:left-60 top-auto relative transform-gpu duration-700 md:w-max ${
+        className={`md:left-16 left-40 mt-16 2xl:left-60 top-auto relative transform-gpu duration-700 md:w-max ${
           slider
             ? "md:translate-y-full md:top-20 md:translate-x-0 -translate-x-full"
             : "md:-translate-y-20 md:-top-20 translate-x-0"
@@ -26,7 +26,7 @@ const NogogeniEvo = () => {
       >
         <img
           className={`transform md:-rotate-90 rotate-0`}
-          width={"950px"}
+          width={"850px"}
           alt=""
           src={el1}
         ></img>
@@ -49,10 +49,10 @@ const NogogeniEvo = () => {
       </div>
       {/* swap */}
       <div
-        className={`text-white md:right-44 w-auto md:w-max md:top-44 top-1/3 absolute 
+        className={`text-white md:right-40 w-auto md:w-max md:top-44 top-1/3 absolute 
         overflow-hidden md:h-3/4 2xl:right-80 inset-y-44 mx-5 2xl:inset-y-56 md:mt-0 -mt-8`}
       >
-        <div className="flex flex-col md:gap-3 gap-1">
+        <div className="flex flex-col md:gap-3 gap-1 text-white">
           <Link to="/" className="flex flex-row cursor-pointer gap-3 w-max">
             <img
               src={back}
@@ -61,10 +61,7 @@ const NogogeniEvo = () => {
             ></img>
             <p className="italic font-medium text-xl my-auto">Back</p>
           </Link>
-          <h2 className="italic text-title">
-            NOGOGENI RACING <br />
-            MARK 1
-          </h2>
+          <h2 className="italic ">NOGOGENI V <br /> EVO</h2>
           <div
             className={`transform-gpu duration-700 md:text-base ${
               slider
@@ -72,7 +69,7 @@ const NogogeniEvo = () => {
                 : "delay-100 -translate-x-0 h-0 opacity-100"
             }`}
           >
-            <p className="text-title font-bold">
+            <p className=" font-bold max-w-md">
               This car uses a 1000W 48V DC brushless motor and a 48V 10Ah
               lithium polymer (LiPO4) battery. Using aluminum alloy as the
               chassis, and carbon fiber.
@@ -80,28 +77,28 @@ const NogogeniEvo = () => {
             <div className="flex flex-row gap-5 mt-2">
               <div className="flex flex-col gap-2">
                 <img alt="" src={steering}></img>
-                <img alt="" src={transmition}></img>
+                <img alt="" src={brake}></img>
               </div>
-              <div className="flex flex-col gap-5 mt-2 italic font-bold text-title">
+              <div className="flex flex-col gap-5 mt-2 italic font-bold">
                 <p>Rack and Pinion Steering System</p>
-                <p>Sprocket &amp; Chain with Manual Shifter</p>
+                <p>Hydraulic Disk Brake System</p>
               </div>
             </div>
             <div className="flex flex-row gap-5 mt-1">
               <div className="flex flex-col">
-                <p className="text-title italic font-bold text title">Wheels</p>
-                <div className="bg-title p-2 md:p-3 flex flex-row gap-3">
-                  <img className="md:py-0 py-1" alt="" src={wheel}></img>
-                  <p className="italic my-auto text-black">6,4" Wide</p>
+                <p className=" italic font-bold text title">
+                  Chassis
+                </p>
+                <div className="bg-title bg-opacity-80 p-2 md:p-3 flex flex-row gap-3">
+                  <img className="md:py-0 py-1" alt="" src={chassis}></img>
+                  <p className="italic my-auto ">Alumunium Alloy</p>
                 </div>
               </div>
               <div className="flex flex-col">
-                <p className="text-title italic font-bold text title">Tires</p>
-                <div className="bg-title md:p-3 p-2 flex flex-row gap-3">
-                  <img alt="" className="md:py-0 py-1" src={breaks}></img>
-                  <p className="italic my-auto text-black">
-                    Hydraulic Disk Brake System
-                  </p>
+                <p className="italic font-bold text title">Body</p>
+                <div className="bg-title bg-opacity-80 md:p-3 p-2 flex flex-row  h-full">
+                  <img alt="" className=" p-2 " src={body1}></img>
+                  <p className="italic my-auto ">Carbon Fiber</p>
                 </div>
               </div>
             </div>
