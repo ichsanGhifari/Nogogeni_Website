0ede2b6ccf1310f3827d49c70112f440f2a3c210
gayuh <gkoma444@gmail.com>
2021-09-05 23:12:13 +0700

    wokring on transition

--- src/components/Staff/StaffElement.jsx
+++ src/components/Staff/StaffElement.jsx
@@ -1,20 +1,28 @@
 import styled from "styled-components";
 
 export const Card = styled.div`
-  cursor: ${({isOpen})=> (isOpen?"text":"pointer")};
-  opacity: 100;
-  -webkit-transition:background 0.5s ease,width 0.4s ease-in-out ;
-  -moz-transition:background 0.5s ease-in,width 0.4s ease-in-out;
-  -o-transition:background 0.5s ease-in,width 0.4s ease-in-out;
-  transition:background 0.4s linear 0.2s ,width 0.4s ease-in-out;
+max-height:85px;
+  cursor: ${({ isOpen }) => (isOpen ? "text" : "pointer")};
+  -webkit-transition: background 0.2s linear 0.3s, width 0.4s ease-in-out,
+    opacity 0.3s ease-in-out;
+  -moz-transition: background 0.2s linear 0.3s, width 0.4s ease-in-out,
+    opacity 0.3s ease-in-out;
+  -o-transition: background 0.2s linear 0.3s, width 0.4s ease-in-out,
+    opacity 0.3s ease-in-out;
+  transition: background 0.2s linear 0.3s, width 0.4s ease-in-out,
+    opacity 0.3s ease-in-out;
   background: ${({ isOpen }) =>
     isOpen
-      ? "linear-gradient(to right, white 2px, transparent 2px) 0 100%, linear-gradient(to right, white 2px, transparent 2px) -583% 100%, linear-gradient(to top, white 2px, transparent 2px) 0% 100%"
-      : ""};    
-  border:${({isOpen})=> (isOpen?"2px solid rgba(255, 255, 255,0)":"2px solid rgba(255, 255, 255,1)")};    
-  border-radius:7px;
+      ? "linear-gradient(to right, rgb(255,255,255,1) 2px, transparent 2px) 0 100%, linear-gradient(to left, rgb(255,255,255,1) 2px, transparent 2px) 95% 100%, linear-gradient(to top, rgb(255,255,255,1) 2px, transparent 2px) 0% 100%"
+      : ""};
+  border: ${({ isOpen }) =>
+    isOpen
+      ? "2px solid rgba(255, 255, 255,0)"
+      : "2px solid rgba(255, 255, 255,1)"};
+
+  border-radius: 7px;
   background-repeat: no-repeat;
-  width:${({isOpen})=> (isOpen?"20rem":" 10rem")};
-  margin:0 auto;
-  background-size: 370px 35px;
+  width: ${({ isOpen }) => (isOpen ? "20rem" : " 10rem")};
+  margin: 0 auto;
+  background-size: 330px 35px;
 `;
